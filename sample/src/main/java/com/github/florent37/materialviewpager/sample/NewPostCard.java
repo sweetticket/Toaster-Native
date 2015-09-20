24c74415d4f50d5f282090e5e3aa17e8ff969689
Jenny Kim <yk494@cornell.edu>
2015-09-20 00:41:50 -0400

    material design theme

---
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/NewPostCard.java
@@ -0,0 +1,45 @@
+package com.github.florent37.materialviewpager.sample;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.florent37.materialviewpager.sample.R;
+
+/**
+ * Created by jennykim on 9/20/15.
+ */
+public class NewPostCard extends RecyclerView.ViewHolder {
+
+    private TextView mCharCount;
+    private EditText mNewPost;
+
+
+    public NewPostCard(View view) {
+        super(view);
+
+        mNewPost = (EditText) view.findViewById(R.id.new_post_input);
+        mNewPost.setFocusable(true);
+        mNewPost.setFocusableInTouchMode(true);
+
+        mCharCount = (TextView) view.findViewById(R.id.char_count);
+        mNewPost.addTextChangedListener(mNewPostWatcher);
+
+    }
+
+    private final TextWatcher mNewPostWatcher= new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //This sets a textview to the current length
+            mCharCount.setText(String.valueOf(140 - s.length()));
+        }
+
+        public void afterTextChanged(Editable s) {
+        }
+    };
+}
---
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/fragment/RecyclerViewFragment.java
@@ -0,0 +1,83 @@
+package com.github.florent37.materialviewpager.sample.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
+import com.github.florent37.materialviewpager.adapter.RecyclerViewMaterialAdapter;
+import com.github.florent37.materialviewpager.sample.R;
+import com.github.florent37.materialviewpager.sample.TestRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by florentchampigny on 24/04/15.
+ */
+public class RecyclerViewFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+//    private TextView mCharCount;
+//    private EditText mNewPost;
+
+    private static final int ITEM_COUNT = 100;
+
+    private List<Object> mContentItems = new ArrayList<>();
+
+    public static RecyclerViewFragment newInstance() {
+        return new RecyclerViewFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recyclerview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new RecyclerViewMaterialAdapter(new TestRecyclerViewAdapter(mContentItems));
+        mRecyclerView.setAdapter(mAdapter);
+
+        {
+            for (int i = 0; i < ITEM_COUNT; ++i)
+                mContentItems.add(new Object());
+            mAdapter.notifyDataSetChanged();
+        }
+
+        MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView, null);
+
+//        mNewPost = (EditText) view.findViewById(R.id.new_post_input);
+//        mCharCount = (TextView) view.findViewById(R.id.char_count);
+//        mNewPost.addTextChangedListener(mNewPostWatcher);
+    }
+
+//    private final TextWatcher mNewPostWatcher= new TextWatcher() {
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//        }
+//
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            //This sets a textview to the current length
+//            mCharCount.setText(String.valueOf(140 - s.length()));
+//        }
+//
+//        public void afterTextChanged(Editable s) {
+//        }
+//    };
+}
